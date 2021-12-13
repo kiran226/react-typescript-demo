fbeadc4b3de5da069b56790a0e01d6c629b1fdba
kiran <gandikotakiran99@gmail.com>
2021-12-13 12:23:55 +0530

    typing an array of object props

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,37 @@
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+
   const personName = {
     first :'Kiran',
     last : 'Gandikota'
   }
+
+  const nameList = [
+    {
+      first : 'Kiran',
+      last : 'Gandikota'
+    },
+    {
+      first :'Harshini',
+      last: 'Gandikota'
+    },
+    {
+      first : 'Gandikota',
+      last : 'Royal'
+    }
+  ]
+
+
   return (
     <div className="App">
       OM Namah Sivayah
       <Greet name='Kiran' messageCount={10} isloggedin={false} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
