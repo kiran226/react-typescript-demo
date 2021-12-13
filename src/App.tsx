c0fa2aecdbcbbf66b732d11537c274d0da6fd3d7
kiran <gandikotakiran99@gmail.com>
2021-12-13 18:12:44 +0530

    useContext hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import './App.css';
 import { Button } from './components/Button';
 import { Container } from './components/Container';
+import { Box } from './components/context/Box';
+import { ThemeContextProvider } from './components/context/ThemeContext';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
 import { Input } from './components/Input';
@@ -69,6 +71,10 @@ function App() {
         <User />
 
         <Counter />
+
+        <ThemeContextProvider>
+          <Box />
+        </ThemeContextProvider>
 
     </div>
   );
