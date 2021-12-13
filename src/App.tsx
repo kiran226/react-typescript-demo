5905cc75bc1c35dc39ab66e5ea2e2bd044a93f36
kiran <gandikotakiran99@gmail.com>
2021-12-13 12:00:17 +0530

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       OM Namah Sivayah
-      <Greet name='Kiran' />
+      <Greet name='Kiran' messageCount={10} isloggedin={false} />
     </div>
   );
 }
