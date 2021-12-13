ed3ffdf943a4060bde83c104541f698a9897349b
kiran <gandikotakiran99@gmail.com>
2021-12-13 12:55:05 +0530

    Advanced props if-else

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
 
@@ -33,6 +34,7 @@ function App() {
       <Greet name='Kiran' messageCount={10} isloggedin={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status="error" />
     </div>
   );
 }
---
+++ src/components/Status.tsx
@@ -0,0 +1,21 @@
+type StatusProps ={
+    status : string
+}
+
+export const Status = (props: StatusProps) => {
+    let message
+    if(props.status==='loading'){
+        message ='Loading...'
+    }
+    else if( props.status === 'success'){
+        message='Data Fetched successfully'
+    }  
+    else if( props.status==='error' ){
+        message ="error fetching data"
+    }  
+    return (
+        <div>
+          <h2 style={{color:'green'}} > Status -{message} </h2>
+        </div>
+    )
+}
