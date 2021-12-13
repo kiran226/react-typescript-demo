384f0490d62cee483ad00fd7a5ade58f7c34a0c7
kiran <gandikotakiran99@gmail.com>
2021-12-13 14:57:47 +0530

    style props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { Button } from './components/Button';
+import { Container } from './components/Container';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
 import { Input } from './components/Input';
@@ -51,11 +52,15 @@ function App() {
       <Button 
         handleClick={(event,id)=>{
         console.log("Button Clicked", event, id ) 
-      }} />
+        }}
+      />
 
       <Input  
         value='' 
-        handleChange={ event => console.log(event)}/>
+        handleChange={ event => console.log(event)}
+      />
+
+      <Container styles={{border: ' 1px solid black' , padding : '1rem'}} />
 
     </div>
   );
