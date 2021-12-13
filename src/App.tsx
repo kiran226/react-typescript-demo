378fd480fb1b0498949d85002076dd8a26480396
kiran <gandikotakiran99@gmail.com>
2021-12-13 12:09:33 +0530

    typing an object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+  const personName = {
+    first :'Kiran',
+    last : 'Gandikota'
+  }
   return (
     <div className="App">
       OM Namah Sivayah
       <Greet name='Kiran' messageCount={10} isloggedin={false} />
+      <Person name={personName} />
     </div>
   );
 }
