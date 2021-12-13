192d632f7efe99574c512f9222eb4e243f961acb
kiran <gandikotakiran99@gmail.com>
2021-12-13 13:16:06 +0530

    nesting children

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css';
 import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
@@ -35,6 +37,12 @@ function App() {
       <Person name={personName} />
       <PersonList names={nameList} />
       <Status status="success" />
+
+      <Heading>Placeholder exists</Heading>
+
+      <Oscar>
+        <Heading>Oscar Award goes to Kiran Gandikota</Heading>
+      </Oscar>
     </div>
   );
 }
