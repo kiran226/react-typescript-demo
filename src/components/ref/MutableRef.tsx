785d27927779c71ff5c8f6bbba2ddc148c2d77d9
kiran <gandikotakiran99@gmail.com>
2021-12-14 10:11:09 +0530

    MutableRef with null check

--- src/components/ref/MutableRef.tsx
+++ src/components/ref/MutableRef.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {useState,useEffect,useRef} from 'react'
 
 export const MutableRef = () => {
+    const [timer, setTimer]= useState(0);
+    const intervalRef = useRef<number | null>(null);
+
+    const stopTimer=()=>{
+       if(intervalRef.current) window.clearInterval(intervalRef.current);
+    }
+    
+    useEffect(()=>{
+        intervalRef.current = window.setInterval(()=>{
+            setTimer((timer)=>timer+1)
+        },1000);
+        return ()=>{
+            stopTimer();
+        }
+    },[]);
+
     return (
         <div>
-            hello
+            Timer -{timer}
+            <button onClick={()=>stopTimer()}>Stop Timer</button>
         </div>
     )
 }
